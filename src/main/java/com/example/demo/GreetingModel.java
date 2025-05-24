63fee0fec5b9670e8a252f377e5fe719801c3186
Ashen365 <168340373+Ashen365@users.noreply.github.com>
2025-05-24 18:05:25 +0530

    Add Greeting REST API with Spring HATEOAS

---
+++ src/main/java/com/example/demo/GreetingModel.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class GreetingModel {
+    private final String message;
+
+    public GreetingModel(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
